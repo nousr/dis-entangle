04eb2f0a24f5abce456dae7c6ae067e07d5eba54
nousr <51308183+nousr@users.noreply.github.com>
2023-01-27 00:34:29 +0000

    lint helper

--- dis_entangle/helpers.py
+++ dis_entangle/helpers.py
@@ -1,13 +1,19 @@
+"""Helpers for dis-entangle"""
+
 from dis_entangle.data_loader_cache import normalize
 
-class GOSNormalize(object):
+class GOSNormalize():
     '''
     Normalize the Image using torch.transforms
     '''
-    def __init__(self, mean=[0.485,0.456,0.406], std=[0.229,0.224,0.225]):
-        self.mean = mean
-        self.std = std
+
+    def __init__(self, mean=None, std=None):
+        self.mean = mean if mean is not None else [0.485,0.456,0.406]
+        self.std = std if std is not None else [0.229,0.224,0.225]
 
     def __call__(self,image):
         image = normalize(image,self.mean,self.std)
         return image
+
+    def __repr__(self):
+        return self.__class__.__name__ + '(mean={0}, std={1})'.format(self.mean, self.std)
