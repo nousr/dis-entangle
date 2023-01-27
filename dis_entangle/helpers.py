14853b5f776da7c64279c6e6ccb66495db2e661c
nousr <51308183+nousr@users.noreply.github.com>
2023-01-27 00:51:46 +0000

    add predict fn

--- dis_entangle/helpers.py
+++ dis_entangle/helpers.py
@@ -5,6 +5,7 @@ from io import BytesIO
 import numpy as np
 import requests
 import torch
+import torch.nn.functional as F
 from dis_entangle.data_loader_cache import im_preprocess, im_reader, normalize
 from torch import nn
 from torchvision import transforms
@@ -58,4 +59,36 @@ def build_model(hypar, device):
         net.load_state_dict(torch.load(hypar["model_path"] + "/" + hypar["restore_model"], map_location=device))
         net.to(device)
     net.eval()
+
     return net
+
+
+def predict(net, inputs_val, shapes_val, hypar, device):
+    """
+    Given an Image, predict the mask
+    """
+    net.eval()
+
+    if hypar["model_digit"] == "full":
+        inputs_val = inputs_val.type(torch.FloatTensor)
+    else:
+        inputs_val = inputs_val.type(torch.HalfTensor)
+
+    inputs_val_v = torch.Tensor(inputs_val, requires_grad=False).to(device)  # wrap inputs in Variable
+
+    ds_val = net(inputs_val_v)[0]  # list of 6 results
+
+    pred_val = ds_val[0][0, :, :, :]  # B x 1 x H x W    # we want the first one which is the most accurate prediction
+
+    ## recover the prediction spatial size to the orignal image size
+    pred_val = torch.squeeze(
+        F.upsample(torch.unsqueeze(pred_val, 0), (shapes_val[0][0], shapes_val[0][1]), mode="bilinear")
+    )
+
+    ma = torch.max(pred_val)
+    mi = torch.min(pred_val)
+    pred_val = (pred_val - mi) / (ma - mi)  # max = 1
+
+    if device == "cuda":
+        torch.cuda.empty_cache()
+    return (pred_val.detach().cpu().numpy() * 255).astype(np.uint8)  # it is the mask we need
